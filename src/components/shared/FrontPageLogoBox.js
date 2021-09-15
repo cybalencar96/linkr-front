2fe71ba60b6281d6023c81bc625eb997ffd9ca28
Kevinmsouza <kevinm.souza@hotmail.com>
2021-09-15 01:29:37 -0400

    Adicionado responsividade ao FrontPageLogoBox

--- src/components/shared/FrontPageLogoBox.js
+++ src/components/shared/FrontPageLogoBox.js
@@ -19,22 +19,40 @@ const BlackLogoSpace = styled.div`
     height: 100vh;
     background-color: #000;
     margin-top: -72px;
-    
+    @media (max-width: 768px){
+        width: 100vw;
+        height: 175px;
+    }
 `;
 
 const LogoTextBox = styled.div`
     margin: 30vh 8%;
     width: 442px;
+    @media (max-width: 768px){
+        margin: 10px 0;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
 `;
 
 const Logo = styled.h1`
     font-family: 'Passion One', cursive;
     font-weight: bold;
-    font-size: 106px;    
+    font-size: 106px;
+    @media (max-width: 768px){
+        font-size: 76px;
+    }
 `;
 
 const Slogan = styled.p`
     font-family: 'Oswald', sans-serif;
     font-weight: bold;
     font-size: 43px;
+    @media (max-width: 768px){
+        font-size: 23px;
+    }
 `
--- src/components/shared/PageStyled.js
+++ src/components/shared/PageStyled.js
@@ -4,6 +4,10 @@ import styled from "styled-components";
 const PageStyled = styled.div`
     margin-top: 72px;
     display: flex;
-`
+    ${props => props.centralized ? "justify-content: center; align-items: center;": ""}
+    @media (max-width: 768px){
+        flex-direction: column;
+    }
+`;
 
 export default PageStyled;
