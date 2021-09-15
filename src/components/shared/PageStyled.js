be4e9ca8ffcffcfb92c465ec57c3f474b12a5f14
Kevinmsouza <kevinm.souza@hotmail.com>
2021-09-15 07:16:25 -0400

    Corrigido bug onde aparecia uma linha horizontal no deploy do versel

--- src/components/shared/PageStyled.js
+++ src/components/shared/PageStyled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 
 const PageStyled = styled.div`
-    margin-top: 72px;
+    padding-top: 72px;
     display: flex;
     ${props => props.centralized ? "justify-content: center; align-items: center;": ""}
     @media (max-width: 992px){
