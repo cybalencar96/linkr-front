715566b472bdaaa580adf0085f90a340a5dc3bba
Kevinmsouza <kevinm.souza@hotmail.com>
2021-09-17 05:51:21 -0400

    Adicionado armazenamento e recuperação de informações do usuario

--- src/components/Timeline/TimelinePage.js
+++ src/components/Timeline/TimelinePage.js
@@ -11,39 +11,44 @@ import Loading from "../shared/Loading";
 import HashtagsInTranding from "../shared/HashtagsInTranding/HashtagsInTranding";
 
 export default function TimelinePage() {
-    const {userData} = useContext(UserContext);
+    const { userData } = useContext(UserContext);
     const [posts, setPosts] = useState("");
-    
-    const config = {
-        headers: {
-            Authorization: `Bearer ${userData.token}`
-        }
-    }
 
     useEffect(() => {
+        if (userData) {
+            renderPosts();
+        }
+    }, [userData])
+
+    function renderPosts() {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userData.token}`
+            }
+        }
         getPosts(config)
-        .then(res => {
-            setPosts(res.data.posts);
-        })
-        .catch(err => {
-            alert("Houve uma falha ao obter os posts, por favor atualize a página");
-        })
-    },[])
+            .then(res => {
+                setPosts(res.data.posts);
+            })
+            .catch(err => {
+                alert("Houve uma falha ao obter os posts, por favor atualize a página");
+            })
+    }
 
 
     if (!posts) {
-        return 	<Loading/>
+        return <Loading />
     }
 
     return (
         <PageStyled centralized>
             <TimelineContainer>
-                    <div>
+                <div>
                     <Title>timeline</Title>
-                    <PostLink/>   
-                    {posts.length !== 0 ? posts.map(post => <Card post={post}/>) : "Nenhum post encontrado"}
-                    </div>
-                    <HashtagsInTranding />
+                    <PostLink />
+                    {posts.length !== 0 ? posts.map(post => <Card post={post} />) : "Nenhum post encontrado"}
+                </div>
+                <HashtagsInTranding />
             </TimelineContainer>
         </PageStyled>
     )
