d9742c7808e31e1f9803039234c5f5635e454f7d
Kevinmsouza <kevinm.souza@hotmail.com>
2021-09-20 04:36:04 -0400

    Criado caixa de texto de edição de post

--- src/components/shared/Card/Card.js
+++ src/components/shared/Card/Card.js
@@ -1,17 +1,17 @@
-import { CardContainer, LinkContent, CardRigth, CardLeft } from "./CardStyled";
+import { CardContainer, LinkContent, CardRigth, CardLeft, EditPostInput } from "./CardStyled";
 import { Heart, HeartOutline } from 'react-ionicons'
 import UserImage from "../UserImage";
 import HashtagSpan from "../HashtagSpan";
 import { Link } from 'react-router-dom'
 import { useContext, useState } from "react";
 import UserContext from "../../../contexts/UserContext";
-import { sendDislikeRequest, sendLikeRequest, sendDeletePostRequest} from "../../../services/Linkr";
+import { sendDislikeRequest, sendLikeRequest, sendDeletePostRequest } from "../../../services/Linkr";
 import ReactTooltip from "react-tooltip";
-import {FaTrash} from "react-icons/fa";
-import {RiPencilFill} from "react-icons/ri";
+import { FaTrash } from "react-icons/fa";
+import { RiPencilFill } from "react-icons/ri";
 import styled from "styled-components";
 
-export default function Card({post,renderPosts}) {
+export default function Card({ post, renderPosts }) {
     const {
         commentCount,
         id,
@@ -23,16 +23,19 @@ export default function Card({post,renderPosts}) {
         text,
         user
     } = post
-    const [ likesState, setLikesState] = useState(likes.map(like => {
+    const [likesState, setLikesState] = useState(likes.map(like => {
         return {
             userId: like.userId,
             username: like["user.username"]
         }
     }));
-    const [ isLoading, setIsLoading] = useState(false)
-    const { userData} = useContext(UserContext);
+    const [isLoading, setIsLoading] = useState(false)
+    const { userData } = useContext(UserContext);
     const isLiked = isLoading !== likesState.map(like => like.userId).includes(userData.user.id);
-    const [ConfirmDeleteState, setConfirmDeleteState] = useState(false); 
+    const [ConfirmDeleteState, setConfirmDeleteState] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingText, setEditingText] = useState(text);
+
     function renderDescription() {
         const formatedText = text.split(" ").map(word => {
             if (word[0] === "#") {
@@ -43,19 +46,20 @@ export default function Card({post,renderPosts}) {
         })
         return formatedText
     }
-    function toggleLike () {
-        if (isLoading){
+
+    function toggleLike() {
+        if (isLoading) {
             return;
         }
         setIsLoading(true)
-        if(isLiked){
+        if (isLiked) {
             sendDislikeRequest(id, userData.token)
                 .then(res => {
                     setLikesState(res.data.post.likes)
                 })
                 .catch(err => alert(err))
                 .finally(() => setIsLoading(false))
-        }else{
+        } else {
             sendLikeRequest(id, userData.token)
                 .then(res => {
                     setLikesState(res.data.post.likes)
@@ -64,27 +68,28 @@ export default function Card({post,renderPosts}) {
                 .finally(() => setIsLoading(false))
         }
     }
-    function createTooltip(){
+
+    function createTooltip() {
         let tooltip = "";
         const likesiDsList = likesState.map(like => like.userId)
         const isLikedOnServer = likesiDsList.includes(userData.user.id);
-        if(isLikedOnServer){
+        if (isLikedOnServer) {
             const indexOfUser = likesiDsList.indexOf(userData.user.id);
             const OtherUsers = likesState.map((like, i) => i === indexOfUser ? null : like.username)
                 .filter((username) => !!username);
             tooltip += `Você`;
-            if(likesState.length > 1){
+            if (likesState.length > 1) {
                 tooltip += `, ${OtherUsers[0]}`;
             }
-        }else{
-            if(likesState.length > 0){
+        } else {
+            if (likesState.length > 0) {
                 tooltip += likesState[0].username;
             }
-            if(likesState.length > 1){
+            if (likesState.length > 1) {
                 tooltip += `, ${likesState[1].username}`;
             }
         }
-        if(likesState.length > 2){
+        if (likesState.length > 2) {
             tooltip += ` e outras ${likesState.length - 2} ${likesState.length === 3 ? "pessoa" : "pessoas"}`;
         }
         return tooltip;
@@ -93,81 +98,89 @@ export default function Card({post,renderPosts}) {
     function deletePost(postId) {
         setIsLoading(true);
         sendDeletePostRequest(postId, userData.token)
-          .then(() => {
-            setIsLoading(false);
-            setConfirmDeleteState(false);
-            renderPosts();
-          })
-          .catch(() => {
-            setIsLoading(false);
-            setTimeout(() => {
-              alert(
-                "Could not delete your post! Please repeat the procedure."
-              );
-            }, 900);
-            setConfirmDeleteState(false);
-          });
-      }
-    
+            .then(() => {
+                setIsLoading(false);
+                setConfirmDeleteState(false);
+                renderPosts();
+            })
+            .catch(() => {
+                setIsLoading(false);
+                setTimeout(() => {
+                    alert(
+                        "Could not delete your post! Please repeat the procedure."
+                    );
+                }, 900);
+                setConfirmDeleteState(false);
+            });
+    }
+
+    function editPost() {
+
+    }
+
     return (
         <>
-        <Superposition ConfirmDeleteState={ConfirmDeleteState}>
-            <ConfirmDeleteScreen>
-                <p>Tem certeza que deseja <br/> excluir essa publicação?</p>
-                <SuperpositionButtons>
-                    <CancelButton disabled={isLoading ? true : false} onClick={() => setConfirmDeleteState(false)}>
-                        Não, voltar
-                    </CancelButton>
-                    <ConfirmButton disabled={isLoading ? true : false} onClick={() => deletePost(id)}>
-                        {isLoading ? "Excluindo..." : "Sim, excluir"}
-                    </ConfirmButton>
-                </SuperpositionButtons>
-            </ConfirmDeleteScreen>
-        </Superposition>
-        <CardContainer>
-            <CardLeft>
-                <Link to={`/user/${user.id}`}>
-                    <UserImage src={user.avatar}/>
-                </Link>
-                {isLiked ? <Heart color={'#AC0000'} height="30px" width="30px" onClick={toggleLike} style={{cursor: 'pointer'}} /> :
-                 <HeartOutline color={'#00000'} height="30px" width="30px" onClick={toggleLike} style={{cursor: 'pointer'}}/>}
-                <p data-tip={createTooltip()}>{likesState.length} likes</p>
-                <ReactTooltip place="bottom" type="light" effect="solid"/>
-            </CardLeft>
+            <Superposition ConfirmDeleteState={ConfirmDeleteState}>
+                <ConfirmDeleteScreen>
+                    <p>Tem certeza que deseja <br /> excluir essa publicação?</p>
+                    <SuperpositionButtons>
+                        <CancelButton disabled={isLoading ? true : false} onClick={() => setConfirmDeleteState(false)}>
+                            Não, voltar
+                        </CancelButton>
+                        <ConfirmButton disabled={isLoading ? true : false} onClick={() => deletePost(id)}>
+                            {isLoading ? "Excluindo..." : "Sim, excluir"}
+                        </ConfirmButton>
+                    </SuperpositionButtons>
+                </ConfirmDeleteScreen>
+            </Superposition>
+            <CardContainer>
+                <CardLeft>
+                    <Link to={`/user/${user.id}`}>
+                        <UserImage src={user.avatar} />
+                    </Link>
+                    {isLiked ? <Heart color={'#AC0000'} height="30px" width="30px" onClick={toggleLike} style={{ cursor: 'pointer' }} /> :
+                        <HeartOutline color={'#00000'} height="30px" width="30px" onClick={toggleLike} style={{ cursor: 'pointer' }} />}
+                    <p data-tip={createTooltip()}>{likesState.length} likes</p>
+                    <ReactTooltip place="bottom" type="light" effect="solid" />
+                </CardLeft>
 
-            <CardRigth>
-                { userData.user.id !== user.id ? <IconsDiv><Link to={userData.user.id !== user.id ? `/user/${user.id}` : `/my-posts/`}>
-                    <h3 className="username">{user.username}</h3>
-                </Link></IconsDiv> :
-                <IconsDiv>
-                <Link to={userData.user.id !== user.id ? `/user/${user.id}` : `/my-posts/`}>
-                    <h3 className="username">{user.username}</h3>
-                </Link>
-                <div>
-                    <IconEdit />
-                    <IconDelete onClick={() => setConfirmDeleteState(true)}/>
-                </div>
-                </IconsDiv>}
-                <p className="description">{renderDescription()}</p>
-                
-                <a href={link}>
-                    <LinkContent>
-                        <div className="linkContent">
-                            <h3 className="linkTitle">{linkTitle}</h3>
-                            <p className="linkDescription">{linkDescription}</p>
-                            <p className="linkUrl">{link}</p>
-                        </div>
-                        <img src={linkImage}/>
-                    </LinkContent>
-                </a>
-            </CardRigth>
-        </CardContainer>
+                <CardRigth>
+                    {userData.user.id !== user.id ? <IconsDiv><Link to={userData.user.id !== user.id ? `/user/${user.id}` : `/my-posts/`}>
+                        <h3 className="username">{user.username}</h3>
+                    </Link></IconsDiv> :
+                        <IconsDiv>
+                            <Link to={userData.user.id !== user.id ? `/user/${user.id}` : `/my-posts/`}>
+                                <h3 className="username">{user.username}</h3>
+                            </Link>
+                            <div>
+                                <IconEdit onClick={() => setIsEditing(!isEditing)}/>
+                                <IconDelete onClick={() => setConfirmDeleteState(true)} />
+                            </div>
+                        </IconsDiv>}
+
+                    {isEditing ?
+                        <EditPostInput value={editingText} onChange={(e) => setEditingText(e.target.value)} /> :
+                        <p className="description">{renderDescription()}</p>
+                    }
+
+                    <a href={link}>
+                        <LinkContent>
+                            <div className="linkContent">
+                                <h3 className="linkTitle">{linkTitle}</h3>
+                                <p className="linkDescription">{linkDescription}</p>
+                                <p className="linkUrl">{link}</p>
+                            </div>
+                            <img src={linkImage} />
+                        </LinkContent>
+                    </a>
+                </CardRigth>
+            </CardContainer>
         </>
     )
 }
 
 const IconDelete = styled(FaTrash)`
-    margin-left: 25px;
+    margin-left: 15px;
     &:hover{
         color: red;
     }
